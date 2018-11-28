7f44391180d2239e53cb8797445c5db21423c28b
Halil Teker <HTeker@users.noreply.github.com>
2018-11-28 15:13:15 +0100

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,11 @@
 <?php
 
 try{
+    $parts = (parse_url(getenv('DATABASE_URL') ?: 'postgresql://root:@localhost/monteur'));
+    extract($parts);
+    $path = ltrim($path, "/");
     // verbinding met db aanmaken
-    $db = new PDO(getenv('DATABASE_URL'));
+    $db = new PDO("pgsql:host={$host};port={$port};dbname={$path}", $user, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE,
         PDO::ERRMODE_WARNING);
 }catch (PDOException $error)
